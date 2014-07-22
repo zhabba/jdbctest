b8593072ee62c99ae05456916542ad668e95234c
zhabba <zhabba108@gmail.com>
2014-07-23 00:50:49 +0400

    initial commit

---
+++ src/JDBCConnector.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 1995, 2011, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import org.w3c.dom.Document;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+
+public class JDBCConnector {
+
+    public String dbms;
+    public String jarFile;
+    public String dbName;
+    public String userName;
+    public String password;
+    public String urlString;
+
+    private String driver;
+    private String serverName;
+    private int portNumber;
+    private Properties prop;
+
+
+    public JDBCConnector(String propertiesFileName) throws IOException {
+        super();
+        this.setProperties(propertiesFileName);
+    }
+
+    public static boolean ignoreSQLException(String sqlState) {
+        if (sqlState == null) {
+            System.out.println("The SQL state is not defined!");
+            return false;
+        }
+        // X0Y32: Jar file already exists in schema
+        if (sqlState.equalsIgnoreCase("X0Y32"))
+            return true;
+        // 42Y55: Table already exists in schema
+        if (sqlState.equalsIgnoreCase("42Y55"))
+            return true;
+        return false;
+    }
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                if (ignoreSQLException(((SQLException) e).getSQLState()) == false) {
+                    e.printStackTrace(System.err);
+                    System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                    System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                    System.err.println("Message: " + e.getMessage());
+                    Throwable t = ex.getCause();
+                    while (t != null) {
+                        System.out.println("Cause: " + t);
+                        t = t.getCause();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void closeConnection(Connection connArg) {
+        System.out.println("Releasing all open resources ...");
+        try {
+            if (connArg != null) {
+                connArg.close();
+                connArg = null;
+            }
+        } catch (SQLException sqle) {
+            printSQLException(sqle);
+        }
+    }
+
+    private void setProperties(String fileName) throws IOException {
+        this.prop = new Properties();
+        FileInputStream fis = new FileInputStream(fileName);
+        prop.loadFromXML(fis);
+
+        this.dbms = this.prop.getProperty("dbms");
+        this.driver = this.prop.getProperty("driver");
+        this.dbName = this.prop.getProperty("database_name");
+        this.userName = this.prop.getProperty("user_name");
+        this.password = this.prop.getProperty("password");
+        this.serverName = this.prop.getProperty("server_name");
+        this.portNumber = Integer.parseInt(this.prop.getProperty("port_number"));
+
+        System.out.println("Set the following properties:");
+        System.out.println("dbms: " + dbms);
+        System.out.println("driver: " + driver);
+        System.out.println("dbName: " + dbName);
+        System.out.println("userName: " + userName);
+        System.out.println("serverName: " + serverName);
+        System.out.println("portNumber: " + portNumber);
+
+    }
+
+    public Connection getConnectionToDatabase() throws SQLException {
+        {
+            Connection conn = null;
+            Properties connectionProps = new Properties();
+            connectionProps.put("user", this.userName);
+            connectionProps.put("password", this.password);
+
+            // Using a driver manager:
+
+            if (this.dbms.equals("mysql")) {
+//        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                conn =
+                        DriverManager.getConnection("jdbc:" + dbms + "://" + serverName +
+                                        ":" + portNumber + "/" + dbName,
+                                connectionProps);
+                conn.setCatalog(this.dbName);
+            } else if (this.dbms.equals("derby")) {
+//        DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+                conn =
+                        DriverManager.getConnection("jdbc:" + dbms + ":" + dbName, connectionProps);
+            }
+            System.out.println("Connected to database");
+            return conn;
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", this.userName);
+        connectionProps.put("password", this.password);
+
+        String currentUrlString = null;
+
+        if (this.dbms.equals("mysql")) {
+            currentUrlString = "jdbc:" + this.dbms + "://" + this.serverName +
+                    ":" + this.portNumber + "/";
+            conn =
+                    DriverManager.getConnection(currentUrlString,
+                            connectionProps);
+
+            this.urlString = currentUrlString + this.dbName;
+            conn.setCatalog(this.dbName);
+        } else if (this.dbms.equals("derby")) {
+            this.urlString = "jdbc:" + this.dbms + ":" + this.dbName;
+
+            conn =
+                    DriverManager.getConnection(this.urlString +
+                            ";create=true", connectionProps);
+
+        }
+        System.out.println("Connected to database");
+        return conn;
+    }
+
+    public Connection getConnection(String userName,
+                                    String password) throws SQLException {
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", userName);
+        connectionProps.put("password", password);
+        if (this.dbms.equals("mysql")) {
+            conn =
+                    DriverManager.getConnection("jdbc:" + this.dbms + "://" + this.serverName +
+                                    ":" + this.portNumber + "/",
+                            connectionProps);
+            conn.setCatalog(this.dbName);
+        } else if (this.dbms.equals("derby")) {
+            conn =
+                    DriverManager.getConnection("jdbc:" + this.dbms + ":" + this.dbName +
+                            ";create=true", connectionProps);
+        }
+        return conn;
+    }
+}
---
+++ src/JDBCTest.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by zhabba on 22.07.14.
+ */
+public class JDBCTest {
+    public static  void main(String[] args) {
+        JDBCConnector JDBCConnector;
+        Connection myConnection = null;
+        if (args[0] == null) {
+            System.err.println("Properties file not specified at command line");
+            return;
+        } else {
+            try {
+                System.out.println("Reading properties file " + args[0]);
+                JDBCConnector = new JDBCConnector(args[0]);
+            } catch (Exception e) {
+                System.err.println("Problem reading properties file " + args[0]);
+                e.printStackTrace();
+                return;
+            }
+        }
+
+        try {
+            myConnection = JDBCConnector.getConnection();
+            //      JDBCTutorialUtilities.outputClientInfoProperties(myConnection);
+            // myConnection = JDBCConnector.getConnection("root", "root", "jdbc:mysql://localhost:3306/");
+            //       myConnection = JDBCConnector.
+            //         getConnectionWithDataSource(JDBCConnector.dbName,"derby","", "", "localhost", 3306);
+
+
+        } catch (SQLException e) {
+            JDBCConnector.printSQLException(e);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            JDBCConnector.closeConnection(myConnection);
+        }
+    }
+}
